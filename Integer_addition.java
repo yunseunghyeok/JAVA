ca455d4a32aa07a734eb11edd1594fca5cc4f0da
yunseunghyeok <71643070+yunseunghyeok@users.noreply.github.com>
2021-04-02 22:06:23 +0900

    Create Integer_addition.java

---
+++ Integer_addition.java
@@ -0,0 +1,17 @@
+public class Add {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+
+		for(int i = 0; i < args.length; i++) {
+			try {
+				sum += Integer.parseInt(args[i]);
+			}
+			catch (NumberFormatException e) {
+
+			}
+		}
+		System.out.println(sum);
+	}
+}
